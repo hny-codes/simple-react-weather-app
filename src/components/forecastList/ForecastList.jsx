d66a09c3d0033f5d0c3133e73dc0b162cdae92eb
Henry <hnguyhenry@protonmail.com>
2023-04-21 17:35:56 -0700

    Update forecast list for both temperature values

--- src/components/forecastList/ForecastList.jsx
+++ src/components/forecastList/ForecastList.jsx
@@ -1,37 +1,43 @@
 import Forecast from '../forecast/forecast';
+import './forecastList.css';
 
-export const ForecastList = ({ data }) => {
+export const ForecastList = ({ data, isCel }) => {
   return (
-    <div>
+    <div className='forecast-list'>
       <Forecast
         date={data.forecast.forecastday[0].date}
         cel={data.forecast.forecastday[0].day.avgtemp_c}
         fah={data.forecast.forecastday[0].day.avgtemp_f}
         image={data.forecast.forecastday[0].day.condition.icon}
+        isCel={isCel}
       />
       <Forecast
         date={data.forecast.forecastday[1].date}
         cel={data.forecast.forecastday[1].day.avgtemp_c}
         fah={data.forecast.forecastday[1].day.avgtemp_f}
         image={data.forecast.forecastday[1].day.condition.icon}
+        isCel={isCel}
       />
       <Forecast
         date={data.forecast.forecastday[2].date}
         cel={data.forecast.forecastday[2].day.avgtemp_c}
         fah={data.forecast.forecastday[2].day.avgtemp_f}
         image={data.forecast.forecastday[2].day.condition.icon}
+        isCel={isCel}
       />
       <Forecast
         date={data.forecast.forecastday[3].date}
         cel={data.forecast.forecastday[3].day.avgtemp_c}
         fah={data.forecast.forecastday[3].day.avgtemp_f}
         image={data.forecast.forecastday[3].day.condition.icon}
+        isCel={isCel}
       />
       <Forecast
         date={data.forecast.forecastday[4].date}
         cel={data.forecast.forecastday[4].day.avgtemp_c}
         fah={data.forecast.forecastday[4].day.avgtemp_f}
         image={data.forecast.forecastday[4].day.condition.icon}
+        isCel={isCel}
       />
     </div>
   );
